e31c7fafdc4039ce3453498cf908ca91b296f7f9
mrjordantanner <mrjordantanner@gmail.com>
2021-04-23 10:10:14 -0500

    Edit CORS

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const io = require('socket.io')(server, {
+    cors: {
+    //   origin: '*',
+       origin: "http://localhost:3000"
+    }
+});
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+
 // Controllers
 const usersController = require('./controllers/users');
 app.use('/users', usersController);
@@ -21,39 +33,11 @@ app.use('/channels', channelsController);
 const messagesController = require('./controllers/messages');
 app.use('/messages', messagesController);
 
-app.options('*', cors());
 
-// const io = require('socket.io')(server, {
-//     cors: {
-//       origin: '*',
-//     //   origin: "http://localhost:3000" || 'https://jts-clique.herokuapp.com/',
-//     // origin: 'https://jts-clique.herokuapp.com/'
-//     }
-// });
-
-const io = require('socket.io')(server);
-
-app.set('socketio', io);
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Origin', 'https://jts-clique.herokuapp.com/');
-    next();
-})
 
 server.listen(port, () => {
     console.log(`server listening on *:${port}`);
 });
-
-// const { database } = require('./db/connection'); 
-
-// function getChannels() {
-//     const cursor =  database.collection('channels').find();
-//             return cursor.map((channel) => {
-//             console.log(`Channel: ${channel.name}`)
-// 	});
-// }
-
 
 io.on('connection', (socket) => { 
     socket.emit('connection', null);
@@ -65,25 +49,7 @@ io.on('connection', (socket) => {
 
     socket.on('channel-join', id => {
 
-        // getChannels().forEach(channel => {
-        //     if (channel._id === id) {
-        //         if (channel.sockets.indexOf(socket._id) == (-1)) {
-        //             channel.sockets.push(socket._id);
-        //             channel.participants = channel.sockets.length;
-        //             io.emit('channel', channel);
-        //             console.log(`Joined ${channel.name} channel.`);
-        //         }
-        //     } else {
-        //         let index = channel.sockets.indexOf(socket._id);
-        //         if (index != (-1)) {
-        //             channel.sockets.splice(index, 1);
-        //             channel.participants = channel.sockets.length;
-        //             io.emit('channel', channel);
-        //             console.log(`Left ${channel.name} channel.`);
-        //         }
-        //     }
-        // });
-        // return id;
+        // TODO: add join/leave code here - use socket.io rooms?
     });
 
     // 2) Listen for send-message event from one client and emit it to all other connected clients
@@ -93,14 +59,9 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        // getChannels().forEach(channel => {
-        //     let index = channel.sockets.indexOf(socket._id);
-        //     if (index != (-1)) {
-        //         channel.sockets.splice(index, 1);
-        //         channel.participants = channel.sockets.length;
-        //         io.emit('channel', channel);
-        //     }
-        // });
+
+        //  io.emit('channel', channel);
+
     });
 });
 //#endregion
